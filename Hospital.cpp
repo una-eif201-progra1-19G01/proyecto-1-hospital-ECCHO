e899b3ab365d6d339b9f016206e4ff1d2774e1a5
Greyvin Tercero Ruiz <53482798+Origamy4321@users.noreply.github.com>
2019-08-27 09:54:06 -0500

    Archivos iniciales.

---
+++ Hospital.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lady Asura III on 27/8/2019.
+//
+
+#include "Hospital.h"
---
+++ Hospital.h
@@ -0,0 +1,14 @@
+//
+// Created by Lady Asura III on 27/8/2019.
+//
+
+#ifndef HOSPITAL_HOSPITAL_H
+#define HOSPITAL_HOSPITAL_H
+
+
+class Hospital {
+
+};
+
+
+#endif //HOSPITAL_HOSPITAL_H
