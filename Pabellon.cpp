04affb7b6a330c2769e72dddcef50f652c9d85d9
NoirRM907A <53484018+NoirRM907A@users.noreply.github.com>
2019-09-05 18:15:24 -0600

    agregar clase pabellon

---
+++ Pabellon.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include"Pabellon.h"
+using::std::string;
+
+Pabellon::Pabellon() {}
+Pabellon::Pabellon() {}
+Pabellon::~Pabellon() {}
---
+++ Pabellon.h
@@ -0,0 +1,24 @@
+#ifndef PABELLON_H
+#define PABELLON_H
+#include <string>
+class Pabellon {
+private:
+	int pabellon[];
+
+public:
+	Pabellon() {}
+	Pabellon() {}
+	~Pabellon() {}
+
+
+
+
+
+
+
+
+
+
+
+};
+#endif //PABELLON_H
