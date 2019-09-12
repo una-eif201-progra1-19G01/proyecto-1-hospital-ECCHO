194cec1c2f733db1d13f1f151442b29802246070
NoirRM907A <53484018+NoirRM907A@users.noreply.github.com>
2019-09-11 20:18:09 -0600

    elaborando pabellon

--- Pabellon.h
+++ Pabellon.h
@@ -1,9 +1,19 @@
 #ifndef PABELLON_H
 #define PABELLON_H
 #include <string>
+#include "Cama.h"
+#include "Paciente.h"
+class Cama;
+class Paciente;
+
 class Pabellon {
 private:
-	int pabellon[];
+	Pabellon **pPabellon;
+	int cantidad;
+	int tamano;
+	char letra;
+	string sexo;
+	int numCamas;
 
 public:
 	Pabellon() {}
